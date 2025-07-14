433baa6d9b4f1a18a70499e27e2c3e757c71cce2
Alan Linares <alanzl2x10@hotmail.com>
2025-07-14 16:08:46 -0300

    feed-modal-p1

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { PHOTO_GET } from "../../api";
+import Error from "../../Help/Error";
+import UseFetch from "../../Hooks/UseFetch";
+import Loading from "../../Help/Loading";
+import style from "./FeedModal.module.css";
+import PhotoContent from "../Photo/PhotoContent";
 
-const FeedModal = () => {
+const FeedModal = ({ photo }) => {
+  const { data, erro, loading, request } = UseFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
   return (
-    <div>FeedModal</div>
-  )
-}
+    <div className={`${style.modal} animeLeft`}>
+      {erro && <Error erro={erro} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
+};
 
-export default FeedModal
+export default FeedModal;
