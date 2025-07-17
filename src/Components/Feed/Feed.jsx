dd9f33759f351e230a0968fe8b0fbbd4b2e001f8
Alan Linares <alanzl2x10@hotmail.com>
2025-07-17 20:20:26 -0300

    scroll-completed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -2,13 +2,57 @@ import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
 
-const Feed = () => {
+const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
+  const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfinite] = React.useState(true);
+
+  // utilizando o UseEffect , para criar um scroll e wheel inifinito e removendo o evento
+  React.useEffect(() => {
+    let wait = false;
+    function infiniteScroll() {
+      if (infinite) {
+        // total do tamanho da barra de scroll.
+        const scroll = window.scrollY;
+        // total da altura que voce consegue ver agora.
+        const height = document.body.offsetHeight - window.innerHeight;
+
+        // verifica o scroll e o height
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
+    }
+
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+
+    return () => {
+      window.removeEventListener("scroll", infiniteScroll);
+      window.removeEventListener("wheel", infiniteScroll);
+    };
+  }, [infinite]);
 
   return (
     <>
-      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+
+      {/* fazendo um loop dentro do useState para acessar a pagina com as fotos */}
+      {pages.map((page) => (
+        <FeedPhotos
+          key={page}
+          user={user}
+          page={page}
+          setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
+        />
+      ))}
     </>
   );
 };
